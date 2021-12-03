114820788e5c91cc036d83ce7e768da0d24c75da
Daniel <dtpontesinfo@gmail.com>
2021-12-03 17:33:11 -0300

    Configuração da Entitie e da Injeção do redis.

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -26,11 +26,18 @@ namespace Basket.API
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddStackExchangeRedisCache(options => 
+            {
+                options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
+            });
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Basket.API", Version = "v1" });
             });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
